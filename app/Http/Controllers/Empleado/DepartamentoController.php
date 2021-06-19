e11aa2411d43e6ba9ccbbab0e56dc7ef8726e905
Mai754 <maicolhertnandez@gmail.com>
2021-06-18 18:36:49 -0600

    Commit Inicial

--- app/Http/Controllers/Empleado/DepartamentoController.php
+++ app/Http/Controllers/Empleado/DepartamentoController.php
@@ -9,81 +9,40 @@ use Illuminate\Http\Request;
 
 class DepartamentoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $departamentos = Departamento::orderby('id')->get();
         return view('empleados.departamento.index', compact('departamentos'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function crear()
     {
         return view('empleados.departamento.crear');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function guardar(ValidacionDepartamento $request)
     {
         Departamento::create($request->all());
         return redirect('empleado/departamento')->with('mensaje', 'Departamento creado con exito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function ver($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function editar($id)
     {
         $departamentos = Departamento::findOrFail($id);
         return view('empleados.departamento.editar', compact('departamentos'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function actualizar(ValidacionDepartamento $request, $id)
     {
         Departamento::findOrFail($id)->update($request->all());
         return redirect('empleado/departamento')->with('mensaje', 'Departamento actualizado con exito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function eliminar(Request $request, $id)
     {
         if($request->ajax()){
