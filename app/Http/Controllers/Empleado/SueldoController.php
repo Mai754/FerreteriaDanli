b1082c681082dcd68a016370c7266c91fc72758b
Mai754 <maicolhertnandez@gmail.com>
2021-06-28 21:49:31 -0600

    Commit Inicial

---
+++ app/Http/Controllers/Empleado/SueldoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Empleado;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ValidacionSueldo;
+use Illuminate\Http\Request;
+
+class SueldoController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function crear()
+    {
+        //
+    }
+    public function guardar(ValidacionSueldo $request)
+    {
+        //
+    }
+    public function ver($id)
+    {
+        //
+    }
+    public function editar($id)
+    {
+        //
+    }
+    public function actualizar(ValidacionSueldo $request, $id)
+    {
+        //
+    }
+    public function eliminar($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Empleado/Sueldo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Empleado;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sueldo extends Model
+{
+    use HasFactory;
+}
