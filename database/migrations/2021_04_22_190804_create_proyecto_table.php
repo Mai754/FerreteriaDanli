28107b5ecf87d32adee091417f0377af0dc48030
Mai754 <maicolhertnandez@gmail.com>
2021-07-25 23:09:47 -0600

    Commit Inicial

--- database/migrations/2021_04_22_190804_create_proyecto_table.php
+++ database/migrations/2021_04_22_190804_create_proyecto_table.php
@@ -12,12 +12,6 @@ class CreateProyectoTable extends Migration
             $table->increments('id');
             $table->string('nombre_del_proyecto', 20);
             $table->string('descripcion', 500);
-            $table->unsignedInteger('cliente_id');
-            $table->foreign('cliente_id', 'fk_proyecto_cliente')->references('id')->on('cliente')->onDelete('restrict')->onUpdate('restrict');
-            $table->unsignedInteger('empleado_id');
-            $table->foreign('empleado_id', 'fk_proyecto_empleado')->references('id')->on('empleado')->onDelete('restrict')->onUpdate('restrict');
-            $table->unsignedInteger('estado_id');
-            $table->foreign('estado_id', 'fk_proyecto_estado')->references('id')->on('estado')->onDelete('restrict')->onUpdate('restrict');
             $table->double('presupuesto');
             $table->double('cantidad_gastada');
             $table->integer('duracion_del_proyecto');
